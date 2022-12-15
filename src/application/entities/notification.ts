858825a197443597fc402a49eee52b6209905b70
luanrem <luanrem@gmail.com>
2022-12-14 21:46:42 -0300

    finish database connection prisma config

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'node:crypto';
 import { Replace } from 'src/helpers/Replace';
 import { Content } from './content';
 
@@ -10,13 +11,19 @@ export interface NotificationProps {
 }
 
 export class Notification {
+  private _id: string;
   private props: NotificationProps;
 
   constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
     };
+  }
+
+  public get id() {
+    return this._id;
   }
 
   public set recipientId(recipientId: string) {
@@ -51,7 +58,7 @@ export class Notification {
     return this.props.readAt;
   }
 
-  public get createdAt(): Date | null | undefined {
-    return this.props.readAt;
+  public get createdAt(): Date {
+    return this.props.createdAt;
   }
 }
